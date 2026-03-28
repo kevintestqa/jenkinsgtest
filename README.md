8a98fff07b599a8ae71c62dfb2cf3fe84c3352ca
Kevin Willocks <kevinwillocks@gmail.com>
2026-03-28 14:34:13 -0500

    Added new evidence

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-https://github.com/kevintestqa/armageddon
+
+# Jenkins S3 Test
+
+Repository: https://github.com/kevintestqa/jenkinsgtest.git
+
+## Screenshots
+
+### Pipeline created
+![Pipeline created](./jenkins_pipeline_newlycreated.png)
+
+### Pipeline trigger
+![Pipeline trigger](./jenkins_pipeline_trigger.png)
+
+### Trigger details
+![Trigger details](./jenkins_pipeline_trigger_details.png)
+
+### Pipeline status
+![Pipeline status](./jenkins_pipeline_status.png)
--- s3.tf
+++ s3.tf
@@ -1,6 +1,6 @@
 
 resource "aws_s3_bucket" "jenkins_g_test" {
-  bucket = "jenkinstestwillocks"
+  bucket        = "jenkinstestwillocks"
   force_destroy = false
   
 
@@ -9,13 +9,29 @@ resource "aws_s3_bucket" "jenkins_g_test" {
   }
 }
 
-
-resource "aws_s3_bucket_public_access_block" "satellite_alb_logs_pab01" {
+resource "aws_s3_bucket_public_access_block" "jenkins_g_test_pab" {
   bucket                  = aws_s3_bucket.jenkins_g_test.id
   block_public_acls       = false
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "jenkins_g_test_public_read" {
+  bucket = aws_s3_bucket.jenkins_g_test.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.jenkins_g_test.arn}/*"
+      }
+    ]
+  })
 }
 
 resource "aws_s3_object" "readme_upload" {
@@ -31,5 +47,29 @@ resource "aws_s3_object" "newly_created_pipeline_upload" {
   key          = "jenkins_pipeline_newlycreated.png"
   source       = "${path.module}/jenkins_pipeline_newlycreated.png"
   etag         = filemd5("${path.module}/jenkins_pipeline_newlycreated.png")
+  content_type = "image/png"
+}
+
+resource "aws_s3_object" "pipeline_trigger_upload" {
+  bucket       = aws_s3_bucket.jenkins_g_test.id
+  key          = "jenkins_pipeline_trigger.png"
+  source       = "${path.module}/jenkins_pipeline_trigger.png"
+  etag         = filemd5("${path.module}/jenkins_pipeline_trigger.png")
+  content_type = "image/png"
+}
+
+resource "aws_s3_object" "pipeline_trigger_details_upload" {
+  bucket       = aws_s3_bucket.jenkins_g_test.id
+  key          = "jenkins_pipeline_trigger_details.png"
+  source       = "${path.module}/jenkins_pipeline_trigger_details.png"
+  etag         = filemd5("${path.module}/jenkins_pipeline_trigger_details.png")
+  content_type = "image/png"
+}
+
+resource "aws_s3_object" "pipeline_trigger_status_upload" {
+  bucket       = aws_s3_bucket.jenkins_g_test.id
+  key          = "jenkins_pipeline_status.png"
+  source       = "${path.module}/jenkins_pipeline_status.png"
+  etag         = filemd5("${path.module}/jenkins_pipeline_status.png")
   content_type = "image/png"
 }
