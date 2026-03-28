fe8db7864f4ab64dab51e13e2eb9bab9c757d4e2
Kevin Willocks <kevinwillocks@gmail.com>
2026-03-28 14:00:30 -0500

    Updated readme

--- s3.tf
+++ s3.tf
@@ -1,7 +1,7 @@
 
 resource "aws_s3_bucket" "jenkins_g_test" {
   bucket = "jenkinstestwillocks"
-  force_destroy = true
+  force_destroy = false
   
 
   tags = {
@@ -16,4 +16,12 @@ resource "aws_s3_bucket_public_access_block" "satellite_alb_logs_pab01" {
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
+}
+
+resource "aws_s3_object" "readme_upload" {
+  bucket       = aws_s3_bucket.jenkins_g_test.id
+  key          = "README.md"
+  source       = "${path.module}/README.md"
+  etag         = filemd5("${path.module}/README.md")
+  content_type = "text/markdown"
 }
